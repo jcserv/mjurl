16db7d78040d68701b651834724420a634d34397
jcserv <jarrod.servilla@gmail.com>
2024-01-05 03:36:22 -0500

    refactor: change tests to tabular

--- internal/transport/api/api_test.go
+++ internal/transport/api/api_test.go
@@ -4,20 +4,72 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+type APITestExpectations struct {
+	code         int
+	header       http.Header
+	responseBody any
+}
+
 func Test_CreateURL(t *testing.T) {
-	api := NewAPI()
-	r := api.RegisterRoutes()
-	req, _ := http.NewRequest(http.MethodPost, APIV1URLPath, nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
+	tests := []struct {
+		name      string
+		inputFunc func(t *testing.T) *http.Request
+		expected  APITestExpectations
+	}{
+		{
+			name: "simple test",
+			inputFunc: func(t *testing.T) *http.Request {
+				t.Helper()
+				req, _ := http.NewRequest(http.MethodPost, APIV1URLPath, nil)
+				return req
+			},
+			expected: APITestExpectations{
+				code: http.StatusOK,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			api := NewAPI()
+			r := api.RegisterRoutes()
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, test.inputFunc(t))
+			assert.Equal(t, test.expected.code, w.Code)
+		})
+	}
 }
 
 func Test_GetURL(t *testing.T) {
-	api := NewAPI()
-	r := api.RegisterRoutes()
-	req, _ := http.NewRequest(http.MethodGet, APIV1URLPath, nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
+	tests := []struct {
+		name      string
+		inputFunc func(t *testing.T) *http.Request
+		expected  APITestExpectations
+	}{
+		{
+			name: "simple test",
+			inputFunc: func(t *testing.T) *http.Request {
+				t.Helper()
+				req, _ := http.NewRequest(http.MethodGet, APIV1URLPath, nil)
+				return req
+			},
+			expected: APITestExpectations{
+				code: http.StatusOK,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			api := NewAPI()
+			r := api.RegisterRoutes()
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, test.inputFunc(t))
+			assert.Equal(t, test.expected.code, w.Code)
+		})
+	}
 }
--- internal/url/service_test.go
+++ internal/url/service_test.go
@@ -1,15 +1,56 @@
 package url
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_URLService_GetURLByShort(t *testing.T) {
-	assert.Equal(t, 1, 1)
+	tests := []struct {
+		name        string
+		input       ShortURL
+		expected    *URL
+		expectedErr error
+	}{
+		{
+			name:        "simple test",
+			input:       "",
+			expected:    nil,
+			expectedErr: nil,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			s := NewURLService()
+			actual, actualErr := s.GetURLByShort(context.Background(), test.input)
+			assert.Equal(t, test.expected, actual)
+			assert.Equal(t, test.expectedErr, actualErr)
+		})
+	}
 }
 
 func Test_URLService_ShortenURL(t *testing.T) {
-	assert.Equal(t, 1, 1)
+	tests := []struct {
+		name        string
+		input       LongURL
+		expected    *URL
+		expectedErr error
+	}{
+		{
+			name:        "simple test",
+			input:       "",
+			expected:    nil,
+			expectedErr: nil,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			s := NewURLService()
+			actual, actualErr := s.ShortenURL(context.Background(), test.input)
+			assert.Equal(t, test.expected, actual)
+			assert.Equal(t, test.expectedErr, actualErr)
+		})
+	}
 }
