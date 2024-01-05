6e6570f35716873f2cba35724050dde9ce7918bc
jcserv <jarrod.servilla@gmail.com>
2024-01-04 20:53:02 -0500

    refactor: change to return HandlerFunc, create boilerplate for create url

--- internal/transport/api/api.go
+++ internal/transport/api/api.go
@@ -13,12 +13,21 @@ func NewAPI() *API {
 	return &API{}
 }
 
-func (a *API) GetURL(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
-
 func (a *API) RegisterRoutes() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/", a.GetURL).Methods(http.MethodGet)
+	r.HandleFunc("/v1/url", a.CreateURL()).Methods(http.MethodPost)
+	r.HandleFunc("/v1/url", a.GetURL()).Methods(http.MethodGet)
 	return r
+}
+
+func (a *API) CreateURL() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func (a *API) GetURL() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}
 }
